258e2f74379a6f37eea286f34b298a97dea19de1
Xandaopedregilho <lelevargas2013@gmail.com>
2025-04-25 21:45:27 +0000

    comecei o projeto

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(){
+
+    int idade;
+    char nome[50];
+    int matricula;
+    
+    printf("digite sua idade: ");
+    scanf("%d" , &idade);
+    
+    printf("Digite seu nome: ");
+    scanf("%s" , &nome);
+    
+    printf("Digite sua matricula: ");
+    scanf("%d" , &matricula);
+    
+    printf("Sua idade: %d\n" , idade);
+    printf("Seu nome: %s\n" , nome);
+    printf("Sua matricula %d\n" , matricula);
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+
+
+}
